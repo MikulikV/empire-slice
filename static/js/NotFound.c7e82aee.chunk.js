8a8b3c06c0a3ce63f65203d48fa6665c40a1f970
MikulikV <mikulik1995@outlook.com>
2023-04-01 23:55:30 +0300

    Updates

---
+++ static/js/NotFound.c7e82aee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,r,a){a.r(r);a(2791);var n=a(268),t=a(184);r.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(n.vr,{})})}}}]);
+//# sourceMappingURL=NotFound.c7e82aee.chunk.js.map
